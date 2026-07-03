9ff1948176694b1d62bb07f6c2f510710ada7dc7
Ankit Kumawat <ankitnav1998@gmail.com>
2026-07-04 00:02:57 +0530

    Fix local dev server network bindings

--- run-server.ps1
+++ run-server.ps1
@@ -1,5 +1,5 @@
 Set-Location 'c:\Users\pc\Desktop\omji new site'
 Start-Transcript -Path 'c:\Users\pc\Desktop\omji new site\powershell-log.txt' -Force
 Write-Host "Current Path: $env:Path"
-npm run dev -- --port 4321
+npm run dev -- --host --port 4321
 Stop-Transcript
